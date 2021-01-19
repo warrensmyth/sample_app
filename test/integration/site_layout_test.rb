fdcbb99316a3b339f49e1c7afed5cd0dfc03ae33
Warren Smyth <warrensmyth@me.com>
2021-01-19 13:09:44 -0600

    Add User index with pagination

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,12 +2,30 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+	def setup
+		@user = users(:michael)
+	end
+
+  test "layout links for non logged in users" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path, count: 0
+  end
+
+  test "layout links for logged in users" do
+  	get login_path
+  	log_in_as(@user)
+  	assert_redirected_to @user
+  	follow_redirect!
+  	assert_template 'users/show'
+  	assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
   end
 end
