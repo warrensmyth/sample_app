6a3579f6658a933ad9562f8d688b85a2bdfc0a19
Warren Smyth <warrensmyth@me.com>
2021-01-27 13:42:43 -0600

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'rails',                    '6.1.0'
+gem 'image_processing',         '1.9.3'
+gem 'mini_magick',              '4.9.5'
+gem 'active_storage_validations', '0.8.9'
 gem 'bcrypt',                   '3.1.13'
 gem 'faker',                    '2.11.0'
 gem 'will_paginate',            '3.3.0'
@@ -38,7 +41,8 @@ group :test do
 end
 
 group :production do
-  gem 'pg', '1.2.3'
+  gem 'pg',         '1.2.3'
+  gem 'aws-sdk-s3', '1.87.0', require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -11,41 +11,37 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   	log_in_as(@user)
   	get root_path
   	assert_select 'div.pagination'
+    assert_select 'input[type=file]'
   	# Invalid submission
-  	assert_no_difference 'Micropost.count' do
-  		post microposts_path, params: { micropost: { content: ""} }
-  	end
-  	assert_select 'div#error_explanation'
-  	assert_select 'a[href=?]', '/?page=2' # Correct pagination link
-  	# Valid submission
-  	content = "This micropost really ties the room together"
-  	assert_difference 'Micropost.count', 1 do
-  		post microposts_path, params: { micropost: { content: content} }
-  	end
-  	assert_redirected_to root_url
-  	follow_redirect!
-  	assert_match content, response.body
-  	# Delete post
-  	assert_select 'a', text: 'delete'
-  	assert_difference 'Micropost.count', -1 do
-  		delete micropost_path(@first_micropost)
-  	end
-  	# Visit different user (no delete links)
-  	get user_path(users(:archer))
-  	assert_select 'a', { text: 'delete', count: 0 }
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "" } }
+    end
+    assert_select 'div#error_explanation'
+    assert_select 'a[href=?]', '/?page=2'  # Correct pagination link
+    # Valid submission
+    content = "This micropost really ties the room together"
+    image = fixture_file_upload('test/fixtures/kitten.jpg', 'image/jpeg')
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, params: { micropost:
+                                      { content: content, image: image } }
+    end
+    assert assigns(:micropost).image.attached?
+    follow_redirect!
+    assert_match content, response.body
+    # Delete a post.
+    assert_select 'a', text: 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+    # Visit a different user.
+    get user_path(users(:archer))
+    assert_select 'a', { text: 'delete', count: 0 }
   end
 
   test "micropost count should increment" do
   	log_in_as(@user)
   	get root_path
   	assert_match "#{@user.microposts.count} microposts", response.body
-  	# User with zero microposts
-  	other_user = users(:malory)
-  	log_in_as(other_user)
-  	get root_path
-  	assert_match "0 Microposts", response.body
-  	other_user.microposts.create!(content: "A micropost")
-  	get root_path
-  	assert_match "1 Micropost", response.body
   end
 end
